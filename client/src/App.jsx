720c37f7c2de8090f5ed1c0b345226ffb4f79caa
Evan Yang <71720197+Evany226@users.noreply.github.com>
2025-04-22 16:26:20 -0400

    feat: init
    installed dependencies and setup

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Landing from "./pages/Landing.jsx";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
