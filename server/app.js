7ed79b395737289763b59f77ea642619dfb7e5ac
Evany226 <evany0226@gmail.com>
2025-05-13 21:05:28 -0400

    cors

--- server/app.js
+++ server/app.js
@@ -4,12 +4,9 @@ import redis from "redis";
 import cors from "cors";
 import "./services/scheduler.js";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
 dotenv.config();
 
 const app = express();
-
-app.use(bodyParser.json());
 
 export const client = redis.createClient({
   url: process.env.REDIS_URL || "redis://redis:6379",
@@ -17,14 +14,7 @@ export const client = redis.createClient({
 client.connect().then(() => {});
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 configRoutes(app);
 
